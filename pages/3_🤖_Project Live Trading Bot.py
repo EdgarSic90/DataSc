c30dd18a1ca33048fd08f5cedfc503e872aed66f
EdgarSic90 <e.sicat@trading707.com>
2022-10-21 17:25:00 +0200

    trading bot page

--- pages/3_🤖_Project Live Trading Bot.py
+++ pages/3_🤖_Project Live Trading Bot.py
@@ -7,5 +7,12 @@ Created on Tue Oct 11 15:34:16 2022
 """
 
 import streamlit as st
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 
-st.title('Live trading Bot')
+st.title('Live Trading Bot')
+
+container = st.container()
+
+img = mpimg.imread('Back-Testing-Trading-Bot.png')
+container.write(plt.imshow(img))
