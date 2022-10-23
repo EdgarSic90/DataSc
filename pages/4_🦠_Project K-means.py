716388e9251c0e9d04d824d5259a7d2466ba8c90
EdgarSic90 <e.sicat@trading707.com>
2022-10-23 19:38:36 +0200

    K-means10_

--- pages/4_🦠_Project K-means.py
+++ pages/4_🦠_Project K-means.py
@@ -167,7 +167,7 @@ clf_cluster = clf.cluster_calc(df.values, clf_centroid)
 _class, centroid_list = clf.predict2(df.values)
 
 #container.write(clf.predict(df.values, plot = True))
-container.pyplot(clf.plot_fig(df, _class, centroid_list[-1]))
+container.pyplot(clf.plot_fig2(df, _class, centroid_list[-1]))
 
 df_display = container.checkbox("Display Raw Data", value=True)
 if df_display:
