a11e888f2419de177417b1b8a047fb1d3334aa2f
EdgarSic90 <e.sicat@trading707.com>
2022-10-23 18:54:31 +0200

    K-means3

--- pages/4_🦠_Project K-means.py
+++ pages/4_🦠_Project K-means.py
@@ -104,50 +104,8 @@ class Kmeans:
         sns.set(rc={'figure.figsize':(11.7,8.27)})
         ax = sns.scatterplot(data=Result, x="Annual_Income_(k$)", y="Spending_Score", hue="Class", palette="bright")      # Clusters display
         ax = sns.scatterplot(data=Result2, x="Annual_Income_(k$)", y="Spending_Score", hue="ClassCentoid", palette="dark", s=100, legend=False)   # Centroid display
-        return plt.show()
+        return ax
 
-
-"""
-def run_kmeans(df, n_clusters=2):
-    kmeans = KMeans(n_clusters, random_state=0).fit(df[["Age", "Income"]])
-
-    fig, ax = plt.subplots(figsize=(16, 9))
-
-    ax.grid(False)
-    ax.set_facecolor("#FFF")
-    ax.spines[["left", "bottom"]].set_visible(True)
-    ax.spines[["left", "bottom"]].set_color("#4a4a4a")
-    ax.tick_params(labelcolor="#4a4a4a")
-    ax.yaxis.label.set(color="#4a4a4a", fontsize=20)
-    ax.xaxis.label.set(color="#4a4a4a", fontsize=20)
-    # --------------------------------------------------
-
-    # Create scatterplot
-    ax = sns.scatterplot(
-        ax=ax,
-        x=df.Age,
-        y=df.Income,
-        hue=kmeans.labels_,
-        palette=sns.color_palette("colorblind", n_colors=n_clusters),
-        legend=None,
-    )
-
-    # Annotate cluster centroids
-    for ix, [age, income] in enumerate(kmeans.cluster_centers_):
-        ax.scatter(age, income, s=200, c="#a8323e")
-        ax.annotate(
-            f"Cluster #{ix+1}",
-            (age, income),
-            fontsize=25,
-            color="#a8323e",
-            xytext=(age + 5, income + 3),
-            bbox=dict(boxstyle="round,pad=0.3", fc="white", ec="#a8323e", lw=2),
-            ha="center",
-            va="center",
-        )
-
-    return fig
-"""
 
 
 container = st.container()
