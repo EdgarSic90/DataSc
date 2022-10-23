4d9ed564531e3e77ea85f15b1d2f0bb850da5620
EdgarSic90 <e.sicat@trading707.com>
2022-10-23 17:00:35 +0200

    trading

--- pages/3_🤖_Project Live Trading Bot.py
+++ pages/3_🤖_Project Live Trading Bot.py
@@ -12,6 +12,15 @@ import matplotlib.pyplot as plt
 
 st.title('Live Trading Bot')
 
+st.write("""
+         In this project I've create a robust live trading model placing orders with FTX broker API.
+         A telegram bot for 24/7 (alert and general wallet information as been implemented.
+         You'll find the code on this [github repository](https://github.com/EdgarSic90/DataS/tree/master/LIVE%20TRADING%20BOT), the current strategy was to predict 5 min candles
+         with mutiple features and make trading decisions from it. 
+         To evaluate the strategy & selection of assets a python Backtesting script has been developped as well.
+         """)
+st.write("You can see below a plot of an asset Backtested with a few performance metrics")
+
 container = st.container()
 
 img = mpimg.imread('Back-Testing-Trading-Bot.png')
